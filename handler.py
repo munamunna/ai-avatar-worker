384cc6cdcab4b2eac0f702735600441b929ff054
munamunna <munapm1@gmail.com>
2026-06-02 11:21:45 +0400

    add edge tts

--- handler.py
+++ handler.py
@@ -1,13 +1,34 @@
 import runpod
+import asyncio
+import edge_tts
+
+
+async def generate_audio(text):
+
+    output_file = "speech.mp3"
+
+    communicate = edge_tts.Communicate(
+        text=text,
+        voice="en-US-AriaNeural"
+    )
+
+    await communicate.save(output_file)
+
+    return output_file
+
 
 def handler(job):
 
-    print("Job received")
+    text = job["input"]["text"]
+
+    audio_file = asyncio.run(
+        generate_audio(text)
+    )
 
     return {
-        "message": "Worker is running",
-        "input": job["input"]
+        "audio_file": audio_file
     }
+
 
 runpod.serverless.start(
     {"handler": handler}
